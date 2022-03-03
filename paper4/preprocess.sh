8469dd0028be86087d954db4f3370a90a1d0222a
khee11 <87646049+khee11@users.noreply.github.com>
2022-03-03 13:33:25 +0900

    Delete paper4 directory

--- paper4/preprocess.sh
+++
@@ -1,3 +0,0 @@
-python preprocess.py --data agnews --tokenizer word --seed 1234 --num_label 150 --num_eval 950 --num_unlab 1500 --min_freq 1
-python write_tfrecord.py --data agnews --tokenizer word
-
